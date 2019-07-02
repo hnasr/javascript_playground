8b5f6c62ead4a4ff4b68a5f22f2204080e0437c5
Hussein Nasser <hus.mhd@gmail.com>
2019-07-02 05:29:54 -0700

    push rest of tutorials

---
+++ express-postgres/server.js
@@ -0,0 +1,125 @@
+const {Client} = require("pg")
+const express = require ("express")
+const app = express();
+app.use(express.json())
+
+const client = new Client({
+    "user": "postgres",
+    "password" : "postgres",
+    "host" : "husseinmac",
+    "port" : 5432,
+    "database" : "todo"
+})
+
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
+
+app.get("/todos", async (req, res) => {
+    const rows = await readTodos();
+    res.setHeader("content-type", "application/json")
+    res.send(JSON.stringify(rows))
+})
+
+
+
+app.post("/todos", async (req, res) => {
+    let result = {}
+    try{
+
+        const reqJson = req.body;
+        await createTodo(reqJson.todo)
+        result.success= true;
+    }
+    catch(e){
+        result.success=false;
+    }
+    finally{
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(result))
+    }
+   
+})
+
+
+
+
+
+app.delete("/todos", async (req, res) => {
+    let result = {}
+    try{
+
+        const reqJson = req.body;
+        await deleteTodo(reqJson.id)
+        result.success= true;
+    }
+    catch(e){
+        result.success=false;
+    }
+    finally{
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(result))
+    }
+   
+})
+
+
+app.listen(8080, () => console.log("Web server is listening.. on port 8080"))
+
+start()
+
+async function start() {
+    await connect();
+    /*
+    const todos = await readTodos();
+    console.log(todos)
+
+    const successCreate = await createTodo("Go to trader joes")
+    console.log(`Creating was ${successCreate}`)
+
+    const successDelete = await deleteTodo(1)
+    console.log(`Deleting was ${successDelete}`)
+    */
+}
+
+async function connect() {
+    try {
+        await client.connect();
+    }
+    catch(e) {
+        console.error(`Failed to connect ${e}`)
+    }
+}
+
+async function readTodos() {
+    try {
+    const results = await client.query("select id, text from todos");
+    return results.rows;
+    }
+    catch(e){
+        return [];
+    }
+}
+
+async function createTodo(todoText){
+
+    try {
+        await client.query("insert into todos (text) values ($1)", [todoText]);
+        return true
+        }
+        catch(e){
+            return false;
+        }
+}
+
+
+
+async function deleteTodo(id){
+
+    try {
+        await client.query("delete from todos where id = $1", [id]);
+        return true
+        }
+        catch(e){
+            return false;
+        }
+}
+
--- jspostgresweb/server.js
+++ jspostgresweb/server.js
@@ -1,4 +1,77 @@
-const {client} = require("pg")
-const app = require("express")()
+const {Client} = require("pg")
+const express = require("express")
+const app = express()
+app.use(express.json())
+let client; 
 
-client.connect
+
+app.delete("/todos", async (req, res) => {
+    const {id} = req.body;  
+    const result = await deleteTodo(id)
+    res.setHeader("content-type", "application/json")
+    res.send(JSON.stringify({result: result}))
+
+})
+app.post("/todos", async (req, res) => {
+     
+    const todoText = req.body;
+    const result = await create(todoText.todoText)
+    res.setHeader("content-type", "application/json")
+    res.send(JSON.stringify({result: result}))
+})
+app.get("/todos", async (req,res) => {
+    res.setHeader("content-type", "application/json")
+    const rows = await query()
+    res.send(JSON.stringify(rows))
+})
+app.listen(8080, () => console.log("listening "))
+async function query () {
+    const res = await client.query("select id, text from todos")
+    return res.rows; 
+}
+async function connect () {
+try {
+
+    client = new Client({
+        "host" : "husseinmac",
+        "user": "postgres",
+        "password": "postgres",
+        "database": "todo",
+        "port" : 5432
+    })
+    
+    await client.connect()
+   
+}
+catch(e)
+{
+    console.error(e)
+
+}
+}
+
+
+
+async function deleteTodo(todoId) {
+    try {
+
+        await client.query(`DELETE FROM TODOS where id = $1`, [todoId] )
+        return true
+    }
+    catch (e){
+        return false;
+    }
+}
+
+async function create(todoText) {
+    try {
+
+        await client.query(`INSERT INTO TODOS (text) VALUES ('${todoText}')`)
+        return true
+    }
+    catch (e){
+        return false;
+    }
+}
+connect();
+query();

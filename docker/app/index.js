6c4c6571efc8c148e61acc0185d95d35e04e6009
Hussein Nasser <hus.mhd@gmail.com>
2019-12-22 18:11:42 -0800

    haproxy

--- docker/app/index.js
+++ docker/app/index.js
@@ -10,4 +10,7 @@ res.send(`appid: ${appid} app1 page: says hello!`))
 app.get("/app2", (req,res) => 
 res.send(`appid: ${appid} app2 page: says hello!`))
  
+app.get("/admin", (req,res) => 
+res.send(`appid: ${appid} ADMIN page: very few people should see this`))
+ 
 app.listen(9999, ()=>console.log(`${appid} is listening on 9999`))

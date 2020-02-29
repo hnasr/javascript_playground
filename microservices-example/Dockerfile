d4dcdbbd776a0991aedca1ae72763e215c660f44
Hussein Nasser <hus.mhd@gmail.com>
2020-02-29 12:28:45 -0800

    microservices

---
+++ microservices-example/Dockerfile
@@ -0,0 +1,5 @@
+FROM node:12
+WORKDIR /home/node/app
+COPY app /home/node/app
+RUN npm install
+CMD npm run app
---
+++ microservices-example/app/index.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+const appid = process.env.APPID;
+
+app.get("/", (req,res) => 
+res.send(`appid: ${appid} home page: says hello!`))
+
+app.get("/app1", (req,res) => 
+res.send(`appid: ${appid} app1 page: says hello!`))
+ 
+app.get("/app2", (req,res) => 
+res.send(`appid: ${appid} app2 page: says hello!`))
+ 
+app.get("/admin", (req,res) => 
+res.send(`appid: ${appid} ADMIN page: very few people should see this`))
+ 
+app.listen(appid, ()=>console.log(`${appid} is listening on ${appid}`))

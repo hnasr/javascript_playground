889941970e859e4a3a4f334cbe93243eb010d609
Hussein Nasser <hus.mhd@gmail.com>
2020-03-28 12:07:45 -0700

    lazy

---
+++ lazy-vs-eager-loading/eager.mjs
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+import Express from "express";
+const url = "https://pomber.github.io/covid19/timeseries.json"
+const app = new Express();
+let timeline = null;
+
+async function loadTimeline() {
+    const fromDate = new Date();
+    if (timeline == null) {
+       const r = await fetch(url)
+       timeline = await r.json();
+    }
+    const toDate = new Date();
+    console.log("Loading time: " + (toDate.getTime() - fromDate.getTime()));
+
+    return timeline;
+}
+
+app.get("/:country", async (req, res) => {
+     const result = await loadTimeline();
+      res.send(result[req.params.country])
+});
+
+loadTimeline();
+app.listen(8080, ()=>console.log("listening..8080"))

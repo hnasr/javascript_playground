dd440b2404bf1a5a18a672d07830bbe9a6c382ec
Hussein Nasser <hus.mhd@gmail.com>
2020-04-14 07:57:59 -0700

    proxy

--- http-proxy/index.js
+++ http-proxy/index.js
@@ -1,9 +1,16 @@
 const app = require("express")()
 
 app.get("/", (req, res)=> {
+    
+    if (req.host === "google.com")
+        res.status(301).redirect("https://hnasr.github.io/")
 
+
+    /*
+    res.sendStatus(301)
     console.log(req);
     res.sendFile(`${__dirname}/${req.host}.html`)
+    */
 })
 
 

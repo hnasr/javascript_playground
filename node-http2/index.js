f7abd89f687833b9a28f86f799251bf74ab365b9
Hussein Nasser <hus.mhd@gmail.com>
2020-06-12 07:56:28 -0700

    nodejs http/2

---
+++ node-http2/index.js
@@ -0,0 +1,27 @@
+const http2 = require ("http2")
+const fs = require("fs")
+
+const server = http2.createSecureServer({
+    "key": fs.readFileSync("private.pem"),
+    "cert": fs.readFileSync("cert.pem")
+})
+
+server.on("stream", (stream, headers) => {
+    console.log(stream.id);
+    stream.respond({
+        "content-type": "application/json",
+        "status": 200
+    })
+
+    stream.end(JSON.stringify({
+        "user": "Hussein",
+        "id": 823
+    }))
+})
+
+
+server.listen(443);
+console.log("listening on port 443");
+
+
+

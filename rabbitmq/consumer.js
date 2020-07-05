9bea69b69e9f6f8c2471aa5b083d1601c0bcd62c
Hussein Nasser <hus.mhd@gmail.com>
2020-07-05 12:35:52 -0700

    remove

--- rabbitmq/consumer.js
+++ rabbitmq/consumer.js
@@ -4,7 +4,7 @@ connect();
 async function connect() {
 
     try {
-        const amqpServer = "amqp://tkgdqstw:n4jcFi8dJ9FLztUErp_vmwd2y5TPFwsP@gull.rmq.cloudamqp.com/tkgdqstw"        //"amqp://localhost:5672"
+        const amqpServer = "amqp://localhost:5672"
         const connection = await amqp.connect(amqpServer)
         const channel = await connection.createChannel();
         await channel.assertQueue("jobs");

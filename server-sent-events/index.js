865ce3c5fa45c7f72ad2858e16c66f51f296a9ed
Hussein Nasser <hus.mhd@gmail.com>
2020-07-13 15:53:49 -0700

    sse

---
+++ server-sent-events/index.js
@@ -0,0 +1,29 @@
+/* Client Code 
+
+let sse = new EventSource("http://localhost:8080/stream");
+sse.onmessage = console.log
+
+*/
+
+const app = require("express")();
+
+app.get("/", (req, res) => res.send("hello!"));
+
+app.get("/stream", (req,res) => {
+
+    res.setHeader("Content-Type", "text/event-stream");
+    send(res);
+
+})
+
+let i = 0;
+function send (res) {
+    
+    res.write("data: " + `hello!${i++}\n\n`);
+
+
+    setTimeout(() => send(res), 1000);
+}
+
+app.listen(8080)
+console.log("Listening on 8080")

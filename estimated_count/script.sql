367e632a86bd71522d45ac28f08915779ed9fff2
Hussein Nasser <hus.mhd@gmail.com>
2021-04-25 10:04:35 -0700

    add index, refactor, add sum of grades

--- estimated_count/script.sql
+++ estimated_count/script.sql
@@ -1,3 +1,5 @@
 create table grades (id serial, g int);
+-- create an index to speed up count queries (I missed this on the video)
+create index g_idx on (g);
 insert into grades (g) select random()*100 from generate_series(1,10000000);
 
